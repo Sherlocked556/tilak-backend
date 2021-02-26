749370a0441bfe5a7dd7b2ffb7441cd7965e84eb
Nikhil Gautam <nikhilgautam992@gmail.com>
2021-02-26 22:21:12 +0530

    inprogress product do-over

--- src/index.server.js
+++ src/index.server.js
@@ -54,7 +54,8 @@ app.use(
     ],
   })
 );
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
